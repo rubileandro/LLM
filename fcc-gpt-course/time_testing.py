663d78bd41c6041a0a3085bce2e710c61711808a
Rubi Leandro <rubi@brislandventures.com>
2023-08-31 17:41:18 +0400

    first commit

---
+++ fcc-gpt-course/time_testing.py
@@ -0,0 +1,12 @@
+import time
+
+
+start_time = time.time()
+
+for i in range(10000):
+    print(i*2)
+
+end_time = time.time()
+
+total_time = end_time - start_time
+print(f"elapsed time: {total_time}")
